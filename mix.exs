287784be60a574f24b867f5a782f56644a8f13fe
Richard Howard <richard.howard@wonderschool.com>
2024-10-26 14:02:31 -0400

    Add jason, dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ChromaBabel.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -21,10 +22,12 @@ defmodule ChromaBabel.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.4"},
       {:nimble_parsec, "~> 1.4"},
 
       # Non-production deps
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
